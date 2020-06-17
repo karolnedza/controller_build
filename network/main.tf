3f3052d78f5a66d4755a9355bd265c1a9b916932
Karol Nedza <knedza@aviatrix.com>
2020-06-17 14:04:59 +0200

    Removed password from network/main.tf

--- network/main.tf
+++ network/main.tf
@@ -9,16 +9,16 @@ data "terraform_remote_state" "controller" {
 provider "aviatrix" {
   username = "admin"
   controller_ip = data.terraform_remote_state.controller.outputs.controller_public_ip
-  password = "Password123"
+  password = xxxxxxx
 }
 
-
-resource "aviatrix_vpc" "aws_vpc" {
-  cloud_type           = 1
-  account_name         = "aws-account"
-  region               = "us-west-1"
-  name                 = "aws-vpc"
-  cidr                 = "10.0.0.0/16"
-  aviatrix_transit_vpc = false
-  aviatrix_firenet_vpc = false
-}
+# 
+# resource "aviatrix_vpc" "aws_vpc" {
+#   cloud_type           = 1
+#   account_name         = "aws-account"
+#   region               = "us-west-1"
+#   name                 = "aws-vpc"
+#   cidr                 = "10.0.0.0/16"
+#   aviatrix_transit_vpc = false
+#   aviatrix_firenet_vpc = false
+# }
